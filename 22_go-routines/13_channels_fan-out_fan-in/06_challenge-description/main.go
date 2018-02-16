c25ba48cf22d951136d09a4f33ec605a1a7a808d
IsaacLusher <36047248+IsaacLusher@users.noreply.github.com>
2018-02-16 16:56:03 +0000

    added final folders

--- 22_go-routines/13_channels_fan-out_fan-in/06_challenge-description/main.go
+++ 22_go-routines/13_channels_fan-out_fan-in/06_challenge-description/main.go
@@ -2,23 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
 
 	in := gen()
 
-	f := factorial(in)
+	c0 := factorial(in)
+	c1 := factorial(in)
+	c2 := factorial(in)
+	c3 := factorial(in)
+	c4 := factorial(in)
+	c5 := factorial(in)
+	c6 := factorial(in)
+	c7 := factorial(in)
+	c8 := factorial(in)
+	c9 := factorial(in)
 
-	for n := range f {
-		fmt.Println(n)
+	var y int
+	for n := range merge(c0, c1, c2, c3, c4, c5, c6, c7, c8, c9) {
+		y++
+		fmt.Println(y, "\t", n)
 	}
 }
 
 func gen() <-chan int {
 	out := make(chan int)
 	go func() {
-		for i := 0; i < 10; i++ {
+		for i := 0; i < 100; i++ {
 			for j := 3; j < 13; j++ {
 				out <- j
 			}
@@ -43,8 +55,31 @@ func fact(n int) int {
 	total := 1
 	for i := n; i > 0; i-- {
 		total *= i
+
 	}
 	return total
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }
 
 /*
